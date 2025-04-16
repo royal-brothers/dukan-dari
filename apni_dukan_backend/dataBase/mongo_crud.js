c37fa6056a8111ef241fb3264011c43e64957316
Govind <141383976+govin07@users.noreply.github.com>
2025-04-16 12:26:03 +0530

    deleteQuery (#1)

--- apni_dukan_backend/dataBase/mongo_crud.js
+++ apni_dukan_backend/dataBase/mongo_crud.js
@@ -22,8 +22,32 @@ return insert_data;
     //todo --> delete get  make by grovo
 
 }
+
+const deleteQuery = async (query_param) => {
+    const { modelName, id, queryType = "deleteOne" } = query_param;
+    let group;
+    let delete_data = "";
+  
+    switch (queryType) {
+      case "deleteMany":
+        group = await mongoose.model(modelName);
+        delete_data = await group.deleteMany(filter);
+        break;
+  
+      default:
+        group = await mongoose.model(modelName);
+        delete_data = await group.deleteOne(id);
+        break;
+    }
+  
+    return delete_data;
+  };
+
 //test
 
 //sadaasd
 
-export {insertQuery}
+export {
+    insertQuery,
+    deleteQuery
+}
