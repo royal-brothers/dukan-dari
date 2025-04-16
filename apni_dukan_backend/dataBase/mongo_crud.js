8cbd5bacc1c6f20900ade076aadf9ef292b78e9e
royal-brothers <apnidukandukan@gmail.com>
2025-04-16 23:30:47 +0530

    feat :- mongo common crud method created

--- apni_dukan_backend/dataBase/mongo_crud.js
+++ apni_dukan_backend/dataBase/mongo_crud.js
@@ -1,53 +1,119 @@
 import mongoose from "mongoose";
 
-const insertQuery = async(query_paraam)=>{
-    
-    const{modelName, data , queryType="insertOne"} = query_paraam;
-   let  group;
+const insertQuery = async (query_paraam) => {
+  const { modelName, data, queryType = "insertOne" } = query_paraam;
+  const groupModel = mongoose.model(modelName);
+  let insert_data = "";
+  switch (queryType) {
+    case "insertMany":
+      insert_data = await groupModel.insertMany(data);
+      break;
 
-    switch(queryType){
-        case "insertMany":
-            let insert_data="";
-             group = await mongoose.model(modelName);
-            insert_data = await group.insertMany(data);
-            break;
-
-         default :
-
-         group = await mongoose.model(modelName);
-         insert_data = await group.create(data);
-         break;
-    }
-return insert_data;
-    //todo --> delete get  make by grovo
-
-}
+    default:
+      insert_data = await groupModel.create(data);
+      break;
+  }
+  return insert_data;
+};
 
 const deleteQuery = async (query_param) => {
-    const { modelName, id, queryType = "deleteOne" } = query_param;
-    let group;
-    let delete_data = "";
-  
-    switch (queryType) {
-      case "deleteMany":
-        group = await mongoose.model(modelName);
-        delete_data = await group.deleteMany(filter);
+  const { modelName, where, queryType = "default" } = query_param;
+  const groupModel = mongoose.model(modelName);
+  let delete_data = "";
+
+  switch (queryType) {
+    case "deleteOne":
+      delete_data = await groupModel.deleteOne(where);
+      break;
+
+    default:
+      group = await mongoose.model(modelName);
+      delete_data = await groupModel.deleteMany(where);
+      break;
+  }
+
+  return delete_data;
+};
+
+const distinctQury = async (query_param) => {
+  const { modelName, distinct_name, where } = query_param;
+  const groupModel = mongoose.model(modelName);
+  const distinct_data = await groupModel.distinct(distinct_name, where);
+  return distinct_data;
+};
+
+const find_all = async (query_param) => {
+  const {
+    modelName,
+    where = {},
+    select = {},
+    sort = {},
+    limit = 0,
+    skip = 0,
+    populateData,
+  } = query_param;
+
+  const groupModel = mongoose.model(modelName);
+  return await groupModel
+    .find(where, select)
+    .sort(sort)
+    .limit(limit)
+    .skip(skip)
+    .populate(populateData)
+    .lean();
+};
+
+const find_one = async(query_param)=>{
+  const {
+    modelName,
+    where = {},
+    select = {},
+    sort = {},
+    limit = 0,
+    skip = 0,
+  } = query_param;
+  const groupModel = mongoose.model(modelName);
+return await groupModel
+.findOne(where , select)
+.sort(sort)
+.limit(limit)
+.skip(skip);
+};
+
+const updateQuery = async(query_param)=>{
+  const {modelName , where , updateData , queryType}=query_param;
+  const groupModel = mongoose.model(modelName);
+  let update = "";
+
+  switch(queryType) {
+    case "updateOne":
+      update = await groupModel.updateOne(where , updateData);
+      break;
+      case "updateMany":
+        update= await groupModel.updateMany(where, updateData);
         break;
-  
-      default:
-        group = await mongoose.model(modelName);
-        delete_data = await group.deleteOne(id);
+        
+        default :
+        update = await groupModel.findOneAndUpdate(where , updateData , {
+          upsert:true,
+          new : true
+        });
         break;
-    }
-  
-    return delete_data;
-  };
+  }
+  return update;
 
-//test
-
-//sadaasd
-
-export {
-    insertQuery,
-    deleteQuery
 }
+
+const count_records = async(query_paraam)=>{
+  const {modelName , condition={}}= query_paraam;
+  const groupModel = mongoose.model(modelName);
+  return await groupModel.countDocuments(condition);
+
+}
+
+const find_distinct = async (query_param) => {
+  const { modelName, distinct_key, where = {} } = query_param;
+  return await mongoose.model(modelName).distinct(distinct_key, where);
+};
+
+export { insertQuery, deleteQuery, distinctQury , find_all, find_one, updateQuery, distinctQury, find_distinct, count_records};
