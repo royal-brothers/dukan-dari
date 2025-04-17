a667dfb7ff4691fa3a53b6041c29f83be3c1ed3c
Govind <141383976+govin07@users.noreply.github.com>
2025-04-17 22:55:08 +0530

    login controller

--- controllers/adminControllers/adminLogin.js
+++ controllers/adminControllers/adminLogin.js
@@ -1,10 +1,25 @@
- import existingAdmin from './adminRegistration.js'
+ import existingAdmin from './adminRegistration.js';
+ import { find_one } from '../../dataBase/mongo_crud.js';
+ import AdminAuth from '../../mongo_model/admin_model/adminAuth.model.js';
  
  const adminLogin = async (req,res) =>{
     
     const {email,password} = req.body;
 
-    if(await existingAdmin())
+    try{
+        if(await existingAdmin(email)){
+            find_one(AdminAuth)
+    
+        }
+    
+    }catch(err){
+        console.error("some error in login user",err)
+        res
+        .status(500)
+        .json({
+         message:"user and password in not metched"
+        })
 
+    }
 
 }
