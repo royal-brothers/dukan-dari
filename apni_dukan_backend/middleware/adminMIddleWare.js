09525fad76ef9617156dca74663a6d1e6cede153
royal-brothers <apnidukandukan@gmail.com>
2025-04-18 01:11:35 +0530

    feat :-genrate admin token

---
+++ apni_dukan_backend/middleware/adminMIddleWare.js
@@ -0,0 +1,37 @@
+import jsonwebToken from "jsonwebtoken";
+
+
+const genToken = async(data)=>{
+    const token = jsonwebToken.sign({data}, process.env.TOKEN_SECRET, {
+        algorithm: "HS512",
+    });
+
+    return token;
+}
+
+const verifyToken  = (req, res, next)=>{
+    try {
+        const authHeader = req.headers.authorization;
+        const token = authHeader && authHeader.split(" ")[1];
+        
+        if(token)
+        {
+            jsonwebToken.verify(token , process.env.TOKEN_SECRET, async(err, tokenData)=>{
+                if (err) {
+                    return res.status(401).json({ message: "Invalid token", error: err });
+                }
+                
+                if (tokenData) {
+                    req.user = tokenData.data;
+                    next();
+                } else {
+                    return res.status(403).json({ message: "Invalid token data" });
+                }
+            });
+        }
+    } catch (error) {
+        
+    }
+}
+
+export {genToken , genToken};
