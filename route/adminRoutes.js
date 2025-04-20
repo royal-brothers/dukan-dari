131ab06a3c801e9b145bfa988ab5188a2307b6d9
Govind <141383976+govin07@users.noreply.github.com>
2025-04-20 13:14:52 +0530

    just test

--- route/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {adminRegistration} from "../controllers/adminControllers/adminRegistration.js";
-
-const adminRouter = express.Router();
-
-adminRouter.post("/register-admin" , adminRegistration );
-
-export {adminRouter};
--- route/index.js
+++
@@ -1,7 +0,0 @@
-import { adminRouter } from "./adminRoutes.js";
-
-const initializeEndPoints = (app) => {
-    app.use("/admin", adminRouter );
-  };
-  
-export default initializeEndPoints;
